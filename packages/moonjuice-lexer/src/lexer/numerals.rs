fb6ddab306b344c3b4e5e6970af71485a111d6d6
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-06 12:08:00 +0100

    Implement better lexing behaviour

--- packages/moonjuice-lexer/src/lexer/numerals.rs
+++ packages/moonjuice-lexer/src/lexer/numerals.rs
@@ -10,28 +10,40 @@ impl Lexer {
 
     let mut requires_digit_for: Option<&str> = None;
 
-    let (supports_float, advance_by, radix) = match self.source.peek(1) {
+    let (supports_float, prefix, allowed_radix) = match self.source.peek(1) {
       Some('x') => {
         requires_digit_for = Some("0x");
-        (false, 2, 16)
+        (false, "0x", 16)
       }
       Some('b') => {
         requires_digit_for = Some("0b");
-        (false, 2, 2)
+        (false, "0b", 2)
       }
-      _ => (true, 0, 10),
+      _ => (true, "", 10),
     };
 
-    self.advance_by(advance_by);
+    self.advance_by(prefix.len());
 
-    let mut is_float = false;
+    let mut num_decimal_points = 0;
+    let mut max_radix = 0;
+    let mut contains_invalid = false;
     let mut numeral = "".to_string();
 
-    while let Some(char) = self.source.peek_next()
-      && char.is_digit(radix)
+    while let Some(char) = self.source.peek_next().cloned()
+      && char.is_ascii_alphanumeric()
     {
-      numeral.push(*char);
+      numeral.push(char);
       self.advance();
+
+      if char.is_digit(2) {
+        max_radix = std::cmp::max(2, max_radix);
+      } else if char.is_digit(10) {
+        max_radix = std::cmp::max(10, max_radix);
+      } else if char.is_digit(16) {
+        max_radix = std::cmp::max(16, max_radix);
+      } else {
+        contains_invalid = true;
+      }
 
       if self.source.is_match("_".chars()) {
         self.advance();
@@ -39,19 +51,7 @@ impl Lexer {
       } else if self.source.is_match(".".chars()) {
         self.advance();
 
-        if !supports_float {
-          return Some(self.new_token(MalformedNumber(
-            "Hex and binary number literals do not support floating point".to_string(),
-          )));
-        }
-
-        if is_float {
-          return Some(self.new_token(MalformedNumber(
-            "Numbers must contain no more than one decimal point".to_string(),
-          )));
-        }
-
-        is_float = true;
+        num_decimal_points += 1;
         requires_digit_for = Some(".");
         numeral.push('.');
       } else {
@@ -59,19 +59,44 @@ impl Lexer {
       }
     }
 
-    if let Some(required_for) = requires_digit_for {
-      return Some(self.new_token(MalformedNumber(format!("A digit must follow '{}'", required_for))));
-    }
-
-    if is_float {
-      match numeral.parse::<f64>() {
-        Ok(value) => Some(self.new_token(Double(value))),
-        Err(err) => Some(self.new_token(MalformedNumber(err.to_string()))),
+    if contains_invalid {
+      Some(self.new_token(MalformedNumber(format!(
+        "Number '{}{}' contains invalid characters",
+        prefix, numeral
+      ))))
+    } else if max_radix > allowed_radix {
+      match max_radix {
+        16 => Some(self.new_token(MalformedNumber(format!(
+          "Number contains hexadecimal digits, did you mean 0x{}?",
+          numeral
+        )))),
+        10 => Some(self.new_token(MalformedNumber(format!(
+          "Number contains decimal digits, did you mean {}?",
+          numeral
+        )))),
+        _ => Some(self.new_token(MalformedNumber(format!("Number '{}' uses unsupported radix", numeral)))),
       }
+    } else if let Some(required_for) = requires_digit_for {
+      Some(self.new_token(MalformedNumber(format!("A digit must follow '{}'", required_for))))
+    } else if num_decimal_points > 0 && !supports_float {
+      Some(self.new_token(MalformedNumber(
+        "Hex and binary number literals do not support floating point".to_string(),
+      )))
+    } else if num_decimal_points > 1 {
+      Some(self.new_token(MalformedNumber(
+        "Numbers must contain no more than one decimal point".to_string(),
+      )))
     } else {
-      match i64::from_str_radix(&numeral, radix) {
-        Ok(value) => Some(self.new_token(Int(value))),
-        Err(err) => Some(self.new_token(MalformedNumber(err.to_string()))),
+      if num_decimal_points > 0 {
+        match numeral.parse::<f64>() {
+          Ok(value) => Some(self.new_token(Double(value))),
+          Err(err) => Some(self.new_token(MalformedNumber(err.to_string()))),
+        }
+      } else {
+        match i64::from_str_radix(&numeral, allowed_radix) {
+          Ok(value) => Some(self.new_token(Int(value))),
+          Err(err) => Some(self.new_token(MalformedNumber(err.to_string()))),
+        }
       }
     }
   }
@@ -142,11 +167,11 @@ mod tests {
     prefix = { "0x", "0b" }
   )]
   fn should_handle_malformed_numbers_when_hex_or_binary_is_float(prefix: &str) {
-    let tokens = Lexer::tokenise(format!("{}1.2", prefix).chars().collect());
+    let tokens = Lexer::tokenise(format!("{}1.1", prefix).chars().collect());
 
     assert_that!(tokens).contains_exactly_in_order(vec![Token {
       value: MalformedNumber("Hex and binary number literals do not support floating point".to_string()),
-      lexeme: format!("{}1.2", prefix),
+      lexeme: format!("{}1.1", prefix),
       start: Position { line: 1, column: 1 },
       end: Position {
         line: 1,
@@ -173,9 +198,9 @@ mod tests {
       "Number contains hexadecimal digits, did you mean 0x12FAB?",
       "Number contains decimal digits, did you mean 0123?",
       "Number contains hexadecimal digits, did you mean 0x12AB?",
-      "Number contains invalid characters",
-      "Number contains invalid characters",
-      "Number contains invalid characters"
+      "Number '1G' contains invalid characters",
+      "Number '0b1G' contains invalid characters",
+      "Number '0x1G' contains invalid characters"
     }
   )]
   fn should_handle_malformed_numbers_when_unsupported_digit_for_radix(lexeme: &str, error: &str) {
