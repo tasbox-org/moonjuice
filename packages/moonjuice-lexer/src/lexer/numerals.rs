3d62b493b74756444d6319a6de7389de0993e6fa
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-06 11:54:09 +0100

    Make errors what we want to happen

--- packages/moonjuice-lexer/src/lexer/numerals.rs
+++ packages/moonjuice-lexer/src/lexer/numerals.rs
@@ -144,48 +144,51 @@ mod tests {
   fn should_handle_malformed_numbers_when_hex_or_binary_is_float(prefix: &str) {
     let tokens = Lexer::tokenise(format!("{}1.2", prefix).chars().collect());
 
-    assert_that!(tokens).contains_exactly_in_order(vec![
-      Token {
-        value: MalformedNumber("Hex and binary number literals do not support floating point".to_string()),
-        lexeme: format!("{}1.", prefix),
-        start: Position { line: 1, column: 1 },
-        end: Position {
-          line: 1,
-          column: prefix.len() + 3,
-        },
+    assert_that!(tokens).contains_exactly_in_order(vec![Token {
+      value: MalformedNumber("Hex and binary number literals do not support floating point".to_string()),
+      lexeme: format!("{}1.2", prefix),
+      start: Position { line: 1, column: 1 },
+      end: Position {
+        line: 1,
+        column: prefix.len() + 4,
       },
-      Token {
-        value: Int(2),
-        lexeme: "2".to_string(),
-        start: Position {
-          line: 1,
-          column: prefix.len() + 3,
-        },
-        end: Position {
-          line: 1,
-          column: prefix.len() + 4,
-        },
-      },
-    ]);
+    }]);
   }
 
   #[test]
   fn should_handle_malformed_numbers_when_multiple_decimal_points() {
     let tokens = Lexer::tokenise("1.2.3.4".chars().collect());
 
-    assert_that!(tokens).contains_exactly_in_order(vec![
-      Token {
-        value: MalformedNumber("Numbers must contain no more than one decimal point".to_string()),
-        lexeme: "1.2.".to_string(),
-        start: Position { line: 1, column: 1 },
-        end: Position { line: 1, column: 5 },
+    assert_that!(tokens).contains_exactly_in_order(vec![Token {
+      value: MalformedNumber("Numbers must contain no more than one decimal point".to_string()),
+      lexeme: "1.2.3.4".to_string(),
+      start: Position { line: 1, column: 1 },
+      end: Position { line: 1, column: 8 },
+    }]);
+  }
+
+  #[parameterized(
+    lexeme = { "12FAB", "0b0123", "0b12AB", "1G", "0b1G", "0x1G" },
+    error = {
+      "Number contains hexadecimal digits, did you mean 0x12FAB?",
+      "Number contains decimal digits, did you mean 0123?",
+      "Number contains hexadecimal digits, did you mean 0x12AB?",
+      "Number contains invalid characters",
+      "Number contains invalid characters",
+      "Number contains invalid characters"
+    }
+  )]
+  fn should_handle_malformed_numbers_when_unsupported_digit_for_radix(lexeme: &str, error: &str) {
+    let tokens = Lexer::tokenise(lexeme.chars().collect());
+
+    assert_that!(tokens).contains_exactly_in_order(vec![Token {
+      value: MalformedNumber(error.to_string()),
+      lexeme: lexeme.to_string(),
+      start: Position { line: 1, column: 1 },
+      end: Position {
+        line: 1,
+        column: lexeme.len() + 1,
       },
-      Token {
-        value: Double(3.4),
-        lexeme: "3.4".to_string(),
-        start: Position { line: 1, column: 5 },
-        end: Position { line: 1, column: 8 },
-      },
-    ]);
+    }]);
   }
 }
