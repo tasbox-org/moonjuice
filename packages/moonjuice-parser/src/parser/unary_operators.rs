09ca5f98f3be78dbfae1733260a812bd1c8ae2d2
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-07 23:12:29 +0100

    Add unary op parsing

---
+++ packages/moonjuice-parser/src/parser/unary_operators.rs
@@ -0,0 +1,35 @@
+use crate::Parser;
+use crate::nodes::expression::Expression::{SyntaxError, UnaryOperator};
+use crate::nodes::expression::ExpressionNode;
+use crate::operators::get_operator_metadata;
+use moonjuice_common::Operator::Subtract;
+use moonjuice_lexer::TokenValue::Operator;
+
+impl Parser {
+  pub(super) fn parse_unary_operator(&mut self) -> ExpressionNode {
+    let start = self.get_start();
+    let token = self.tokens.consume();
+    let end = self.get_end();
+
+    if let Some(token) = token
+      && let Operator(op) = token.value
+      && let metadata = get_operator_metadata(op.clone())
+      && (metadata.is_unary || op == Subtract)
+    {
+      let sub_expression_lhs = self.parse_operand();
+      let rhs = self.parse_sub_expression(sub_expression_lhs, metadata.precedence);
+
+      ExpressionNode {
+        value: UnaryOperator { op, rhs }.into(),
+        start,
+        end,
+      }
+    } else {
+      ExpressionNode {
+        value: SyntaxError("Expected operand or unary operator".to_string()).into(),
+        start,
+        end,
+      }
+    }
+  }
+}
