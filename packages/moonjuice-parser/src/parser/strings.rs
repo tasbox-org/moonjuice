51b47823c063c5d589718da73678a3cfcdace8c3
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-08 14:36:15 +0100

    Implement basic string parsing

--- packages/moonjuice-parser/src/parser/strings.rs
+++ packages/moonjuice-parser/src/parser/strings.rs
@@ -1,6 +1,38 @@
 use crate::Parser;
-use crate::nodes::expression::ExpressionNode;
+use crate::nodes::expression::Expression::SyntaxError;
+use crate::nodes::expression::{Expression, ExpressionNode};
+use moonjuice_lexer::StringTokenType::Whole;
+use moonjuice_lexer::TokenValue;
 
 impl Parser {
-  pub(super) fn parse_string(&mut self) -> ExpressionNode {}
+  pub(super) fn parse_string(&mut self) -> ExpressionNode {
+    let start = self.get_start();
+
+    match self.tokens.consume().map(|token| token.value.clone()) {
+      Some(TokenValue::String(Whole, value)) => ExpressionNode {
+        value: Expression::String {
+          segments: vec![value],
+          arguments: vec![],
+        }
+        .into(),
+        start,
+        end: self.get_end(),
+      },
+      Some(TokenValue::MalformedString(Whole, message)) => ExpressionNode {
+        value: SyntaxError(message).into(),
+        start,
+        end: self.get_end(),
+      },
+      Some(TokenValue::String(_, _) | TokenValue::MalformedString(_, _)) => ExpressionNode {
+        value: SyntaxError("Format strings are not yet implemented into the parser".to_string()).into(),
+        start,
+        end: self.get_end(),
+      },
+      _ => ExpressionNode {
+        value: SyntaxError("Expected string".to_string()).into(),
+        start,
+        end: self.get_end(),
+      },
+    }
+  }
 }
