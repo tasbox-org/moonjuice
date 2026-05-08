f2946f7d6acad22a9ed9e1380e460b2dda4c455a
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-08 12:40:51 +0100

    Implement expression parsing

--- packages/moonjuice-parser/src/parser/expressions.rs
+++ packages/moonjuice-parser/src/parser/expressions.rs
@@ -1,13 +1,60 @@
 use crate::Parser;
-use crate::nodes::expression::{Expression, ExpressionNode};
-use moonjuice_common::Position;
+use crate::nodes::expression::ExpressionNode;
+use crate::operators::get_operator_metadata;
+use moonjuice_common::Operator::OptionalIndex;
+use moonjuice_common::SpecialCharacter::{OpenBracket, OpenSquareBracket};
+use moonjuice_lexer::Token;
+use moonjuice_lexer::TokenValue::{Operator, SpecialCharacter};
+
+fn is_binary_operator_with_greater_or_equal_precedence(token: Token, min_precedence: u16) -> bool {
+  if let Some(metadata) = get_operator_metadata(token)
+    && !metadata.is_unary
+    && metadata.precedence >= min_precedence
+  {
+    true
+  } else {
+    false
+  }
+}
 
 impl Parser {
   pub(super) fn parse_expression(&mut self) -> ExpressionNode {
-    ExpressionNode {
-      value: Expression::SyntaxError("TODO".to_string()).into(),
-      start: Position { line: 0, column: 0 },
-      end: Position { line: 0, column: 0 },
+    let lhs = self.parse_operand();
+    self.parse_sub_expression(lhs, 0)
+  }
+
+  pub(super) fn parse_sub_expression(&mut self, mut lhs: ExpressionNode, min_precedence: u16) -> ExpressionNode {
+    while let Some(lookahead) = self.tokens.peek_next()
+      && is_binary_operator_with_greater_or_equal_precedence(lookahead.clone(), min_precedence)
+    {
+      lhs = match lookahead.value {
+        SpecialCharacter(OpenBracket) => self.parse_call(lhs, false),
+        SpecialCharacter(OpenSquareBracket) => self.parse_index_expression(lhs, false),
+        Operator(OptionalIndex)
+          if self
+            .tokens
+            .peek(1)
+            .is_some_and(|token| token.value == SpecialCharacter(OpenBracket)) =>
+        {
+          self.tokens.consume();
+          self.parse_call(lhs, true)
+        }
+        Operator(OptionalIndex)
+          if self
+            .tokens
+            .peek(1)
+            .is_some_and(|token| token.value == SpecialCharacter(OpenSquareBracket)) =>
+        {
+          self.tokens.consume();
+          self.parse_index_expression(lhs, true)
+        }
+        Operator(_) => self.parse_binary_operator(lhs),
+        _ => panic!(
+          "is_binary_operator_with_greater_or_equal_precedence returned true for token other than binary operator"
+        ),
+      }
     }
+
+    lhs
   }
 }
