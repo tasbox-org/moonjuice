a26630b2582de200e68e48db1319cd7132690e85
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-08 15:07:23 +0100

    more precedence and binding tests

--- packages/moonjuice-parser/src/tests/maths.rs
+++ packages/moonjuice-parser/src/tests/maths.rs
@@ -6,4 +6,8 @@ mod tests {
   snapshot!(should_parse_binary_operation, "1 + 2");
   snapshot!(should_parse_left_associative, "1 + 2 + 3");
   snapshot!(should_parse_right_associative, "1 ?? 2 ?? 3");
+  snapshot!(should_parse_precedence, "1 + 2 * 3");
+  snapshot!(should_parse_mixed_unary_binary_when_binary_higher_precedence, "-a.b");
+  snapshot!(should_parse_mixed_unary_binary_when_binary_lower_precedence, "-a - b");
+  snapshot!(should_parse_bracketed_expression, "1 + (2 + 3)");
 }
