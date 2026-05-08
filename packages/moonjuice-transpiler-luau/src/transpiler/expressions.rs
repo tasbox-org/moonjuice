ee012a99a6ebeb9f06b1e9f09e90c4af616284ef
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-08 21:14:39 +0100

    Implement binary op transpiling

--- packages/moonjuice-transpiler-luau/src/transpiler/expressions.rs
+++ packages/moonjuice-transpiler-luau/src/transpiler/expressions.rs
@@ -19,8 +19,10 @@ impl LuauTranspiler {
       }
       Expression::Symbol(value) => self.emit_symbol(value.as_str()),
       Expression::Block(body) => self.emit_block(body)?,
-      Expression::UnaryOperator { op, rhs } => self.emit_unary_operator(op, rhs)?,
-      Expression::BinaryOperator { op, lhs, rhs } => self.emit_binary_operator(op, lhs, rhs)?,
+      Expression::UnaryOperator { op, rhs } => self.emit_unary_operator(op, rhs, expression.start, expression.end)?,
+      Expression::BinaryOperator { op, lhs, rhs } => {
+        self.emit_binary_operator(op, lhs, rhs, expression.start, expression.end)?
+      }
       Expression::Function { parameters, body } => self.emit_function(parameters, body)?,
       Expression::If {
         if_branches,
@@ -77,9 +79,113 @@ impl LuauTranspiler {
     Ok(())
   }
 
-  fn emit_unary_operator(&mut self, op: Operator, rhs: ExpressionNode) -> Result<(), Error> {}
+  fn emit_unary_operator(
+    &mut self,
+    op: Operator,
+    rhs: ExpressionNode,
+    start: Position,
+    end: Position,
+  ) -> Result<(), Error> {
+    let (prefix, suffix) = match op {
+      Operator::Subtract => ("(-", ")"),
+      Operator::Not => ("(not ", ")"),
+      Operator::BitwiseNot => ("bit32.bnot(", ")"),
+      Operator::Length => ("(#", ")"),
+      _ => {
+        return Err(Error {
+          message: "Invalid AST. UnaryOperator node contains a non-unary Operator (this should never happen!)"
+            .to_string(),
+          start,
+          end,
+        });
+      }
+    };
 
-  fn emit_binary_operator(&mut self, op: Operator, lhs: ExpressionNode, rhs: ExpressionNode) -> Result<(), Error> {}
+    if !self.get_scope().is_in_expression {
+      self.source.push_str("local _ = ");
+    }
+
+    self.push_expression_scope();
+    self.source.push_str(prefix);
+    self.emit_expression(rhs)?;
+    self.source.push_str(suffix);
+    self.pop_scope();
+
+    Ok(())
+  }
+
+  fn emit_binary_operator(
+    &mut self,
+    op: Operator,
+    lhs: ExpressionNode,
+    rhs: ExpressionNode,
+    start: Position,
+    end: Position,
+  ) -> Result<(), Error> {
+    let (prefix, middle, suffix) = match op {
+      Operator::Add => ("(", ") + (", ")"),
+      Operator::Subtract => ("(", ") - (", ")"),
+      Operator::Multiply => ("(", ") * (", ")"),
+      Operator::Divide => ("(", ") / (", ")"),
+      Operator::Modulo => ("(", ") % (", ")"),
+      Operator::Concat => ("(", ") .. (", ")"),
+      Operator::And => ("(", ") and (", ")"),
+      Operator::Or => ("(", ") or (", ")"),
+      Operator::OptionalCoalesce => (
+        "(function() local lhs = (",
+        "); return if lhs == nil then (",
+        ") else lhs end)()",
+      ),
+      Operator::Equals => ("(", ") == (", ")"),
+      Operator::NotEquals => ("(", ") ~= (", ")"),
+      Operator::LessThan => ("(", ") < (", ")"),
+      Operator::GreaterThan => ("(", ") > (", ")"),
+      Operator::LessThanOrEqual => ("(", ") <= (", ")"),
+      Operator::GreaterThanOrEqual => ("(", ") >= (", ")"),
+      Operator::Pipe => {
+        return self.emit_pipe_operator(lhs, rhs);
+      }
+      Operator::Index => ("(", ")[", "]"),
+      Operator::OptionalIndex => (
+        "(function() local lhs = ",
+        "; return if lhs == nil then nil else lhs[",
+        "] end)()",
+      ),
+      Operator::Assignment => {
+        return self.emit_assignment_operator(lhs, rhs);
+      }
+      Operator::BitwiseAnd => ("bit32.band(", ", ", ")"),
+      Operator::BitwiseOr => ("bit32.bor(", ", ", ")"),
+      Operator::BitwiseXor => ("bit32.bxor(", ", ", ")"),
+      Operator::LeftShift => ("bit32.lshift(", ", ", ")"),
+      Operator::RightShift => ("bit32.rshift(", ", ", ")"),
+      _ => {
+        return Err(Error {
+          message: "Invalid AST. BinaryOperator node contains a unary Operator (this should never happen!)".to_string(),
+          start,
+          end,
+        });
+      }
+    };
+
+    if !self.get_scope().is_in_expression {
+      self.source.push_str("local _ = ");
+    }
+
+    self.push_expression_scope();
+    self.source.push_str(prefix);
+    self.emit_expression(lhs)?;
+    self.source.push_str(middle);
+    self.emit_expression(rhs)?;
+    self.source.push_str(suffix);
+    self.pop_scope();
+
+    Ok(())
+  }
+
+  fn emit_assignment_operator(&mut self, lhs: ExpressionNode, rhs: ExpressionNode) -> Result<(), Error> {}
+
+  fn emit_pipe_operator(&mut self, lhs: ExpressionNode, rhs: ExpressionNode) -> Result<(), Error> {}
 
   fn emit_function(&mut self, parameters: Vec<LValueNode>, body: Vec<StatementNode>) -> Result<(), Error> {}
 
