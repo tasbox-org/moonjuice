667aca9afa0577c20784035ae57a8162c4cf9e11
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-08 15:02:16 +0100

    add binary and associativity tests

--- packages/moonjuice-parser/src/tests/maths.rs
+++ packages/moonjuice-parser/src/tests/maths.rs
@@ -3,4 +3,7 @@ mod tests {
   use crate::snapshot;
 
   snapshot!(should_parse_unary_operation, "-5");
+  snapshot!(should_parse_binary_operation, "1 + 2");
+  snapshot!(should_parse_left_associative, "1 + 2 + 3");
+  snapshot!(should_parse_right_associative, "1 ?? 2 ?? 3");
 }
