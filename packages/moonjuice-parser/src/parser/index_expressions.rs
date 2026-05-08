a2d04044064435b8971d0883c71f56d70bdeccb0
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-08 13:38:55 +0100

    implement index expressions

--- packages/moonjuice-parser/src/parser/index_expressions.rs
+++ packages/moonjuice-parser/src/parser/index_expressions.rs
@@ -1,6 +1,45 @@
 use crate::Parser;
+use crate::nodes::expression::Expression::{BinaryOperator, SyntaxError};
 use crate::nodes::expression::ExpressionNode;
+use moonjuice_common::Operator::{Index, OptionalIndex};
+use moonjuice_common::SpecialCharacter::{CloseSquareBracket, OpenSquareBracket};
+use moonjuice_lexer::TokenValue::SpecialCharacter;
 
 impl Parser {
-  pub(super) fn parse_index_expression(&mut self, lhs: ExpressionNode, is_optional: bool) -> ExpressionNode {}
+  pub(super) fn parse_index_expression(&mut self, lhs: ExpressionNode, is_optional: bool) -> ExpressionNode {
+    let start = self.get_start();
+
+    if self
+      .tokens
+      .consume()
+      .is_none_or(|token| token.value != SpecialCharacter(OpenSquareBracket))
+    {
+      return ExpressionNode {
+        value: SyntaxError("Expected '[' to open table index".to_string()).into(),
+        start,
+        end: self.get_end(),
+      };
+    }
+
+    let rhs = self.parse_expression();
+
+    if self
+      .consume_if(|value| value == SpecialCharacter(CloseSquareBracket))
+      .is_none()
+    {
+      return ExpressionNode {
+        value: SyntaxError("Expected ']' to close table index".to_string()).into(),
+        start,
+        end: self.get_end(),
+      };
+    }
+
+    let op = if is_optional { OptionalIndex } else { Index };
+
+    ExpressionNode {
+      value: BinaryOperator { op, lhs, rhs }.into(),
+      start,
+      end: self.get_end(),
+    }
+  }
 }
