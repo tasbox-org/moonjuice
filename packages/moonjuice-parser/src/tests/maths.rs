8a6ed911af7e358a460f4197a4191618fdef093d
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-08 14:59:03 +0100

    add helper macro

--- packages/moonjuice-parser/src/tests/maths.rs
+++ packages/moonjuice-parser/src/tests/maths.rs
@@ -1,9 +1,6 @@
 #[cfg(test)]
 mod tests {
-  use crate::tokenise_and_parse;
+  use crate::snapshot;
 
-  #[test]
-  fn should_parse_unary_operation() {
-    insta::assert_yaml_snapshot!(tokenise_and_parse("-5".chars().collect()))
-  }
+  snapshot!(should_parse_unary_operation, "-5");
 }
