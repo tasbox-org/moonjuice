56130b46a91db68df8ccf9eb5d4e91db2fd62d1d
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-10 18:02:23 +0100

    Set up stdio listener for lsp

--- packages/moonjuice-lsp/src/main.rs
+++ packages/moonjuice-lsp/src/main.rs
@@ -1,12 +1,32 @@
-use tower_lsp_server::ls_types::{InitializeParams, InitializeResult};
-use tower_lsp_server::{LanguageServer, LspService};
+use tower_lsp_server::ls_types::{
+  InitializeParams, InitializeResult, InitializedParams, MessageType, SemanticTokensParams, SemanticTokensResult,
+};
+use tower_lsp_server::{Client, LanguageServer, LspService, Server};
 
 #[derive(Debug)]
-struct Backend {}
+struct Backend {
+  client: Client,
+}
 
 impl LanguageServer for Backend {
   async fn initialize(&self, params: InitializeParams) -> tower_lsp_server::jsonrpc::Result<InitializeResult> {
     Ok(InitializeResult::default())
+  }
+
+  async fn initialized(&self, params: InitializedParams) {
+    self.client.log_message(MessageType::INFO, "Server initialised").await;
+  }
+
+  async fn semantic_tokens_full(
+    &self,
+    params: SemanticTokensParams,
+  ) -> tower_lsp_server::jsonrpc::Result<Option<SemanticTokensResult>> {
+    self
+      .client
+      .log_message(MessageType::INFO, format!("Semantic highlight request:\n{:?}", params))
+      .await;
+
+    Ok(None)
   }
 
   async fn shutdown(&self) -> tower_lsp_server::jsonrpc::Result<()> {
@@ -14,7 +34,12 @@ impl LanguageServer for Backend {
   }
 }
 
-fn main() {
-  let lsp_service = LspService::new(|_client| Backend {});
-  dbg!(lsp_service);
+#[tokio::main]
+async fn main() {
+  let stdin = tokio::io::stdin();
+  let stdout = tokio::io::stdout();
+
+  let (service, socket) = LspService::new(|client| Backend { client });
+
+  Server::new(stdin, stdout, socket).serve(service).await;
 }
