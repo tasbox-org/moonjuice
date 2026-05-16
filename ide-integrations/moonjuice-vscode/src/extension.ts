63ebbd3a1510f3b5ab1f4f72e6d106c64b674d0b
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-16 17:49:10 +0100

    Add basic LSP integration to vscode extensions

--- ide-integrations/moonjuice-vscode/src/extension.ts
+++ ide-integrations/moonjuice-vscode/src/extension.ts
@@ -1,6 +1,29 @@
 import * as vscode from "vscode";
+import { LanguageClient, TransportKind } from "vscode-languageclient/node";
 
-export function activate(context: vscode.ExtensionContext) {
+let client: LanguageClient | undefined;
+
+export async function activate(context: vscode.ExtensionContext) {
+  const lspPath = vscode.workspace.getConfiguration("moonjuice").get("lspPath");
+
+  if (typeof lspPath !== "string" || lspPath.length < 1) {
+    return vscode.window.showErrorMessage("The `#moonjuice.lspPath#` setting was not configured");
+  }
+
+  client = new LanguageClient(
+    "moonjuice",
+    "MoonJuice",
+    {
+      command: lspPath,
+      transport: TransportKind.stdio,
+    },
+    {
+      documentSelector: [{ scheme: "file", language: "moonjuice" }],
+    },
+  );
+
+  await client.start();
+
   const disposable = vscode.commands.registerCommand("moonjuice-vscode.helloWorld", () => {
     return vscode.window.showInformationMessage(vscode.workspace.getConfiguration("moonjuice").get("lspPath") ?? "");
   });
@@ -8,4 +31,7 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export async function deactivate() {
+  await client?.dispose();
+  client = undefined;
+}
