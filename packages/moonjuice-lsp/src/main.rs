52156784f7438db80f1607af380a36cfd68d3179
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-16 19:02:35 +0100

    Implement range highlighting and cache tokens

--- packages/moonjuice-lsp/src/main.rs
+++ packages/moonjuice-lsp/src/main.rs
@@ -1,29 +1,57 @@
+mod document;
 mod semantic_highlighting;
 
-use crate::semantic_highlighting::SemanticHighlightingProvider;
+use crate::document::Document;
+use crate::semantic_highlighting::get_legend;
+use dashmap::DashMap;
 use tower_lsp_server::ls_types::{
-  InitializeParams, InitializeResult, InitializedParams, MessageType, SemanticTokensDeltaParams,
-  SemanticTokensFullDeltaResult, SemanticTokensFullOptions, SemanticTokensOptions, SemanticTokensParams,
-  SemanticTokensRangeParams, SemanticTokensRangeResult, SemanticTokensResult, SemanticTokensServerCapabilities,
+  DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams, DocumentFilter, InitializeParams,
+  InitializeResult, InitializedParams, MessageType, SemanticTokensFullOptions, SemanticTokensOptions,
+  SemanticTokensParams, SemanticTokensRangeParams, SemanticTokensRangeResult, SemanticTokensRegistrationOptions,
+  SemanticTokensResult, SemanticTokensServerCapabilities, ServerCapabilities, TextDocumentRegistrationOptions,
+  TextDocumentSyncCapability, TextDocumentSyncKind, TextDocumentSyncOptions,
 };
 use tower_lsp_server::{Client, LanguageServer, LspService, Server};
 
 struct Backend {
   client: Client,
-  semantic_highlighting_provider: SemanticHighlightingProvider,
+  documents: DashMap<String, Document>,
 }
 
 impl LanguageServer for Backend {
   async fn initialize(&self, _params: InitializeParams) -> tower_lsp_server::jsonrpc::Result<InitializeResult> {
-    let mut result = InitializeResult::default();
-    result.capabilities.semantic_tokens_provider = Some(SemanticTokensServerCapabilities::SemanticTokensOptions(
-      SemanticTokensOptions {
-        work_done_progress_options: Default::default(),
-        legend: self.semantic_highlighting_provider.get_legend(),
-        range: None,
-        full: Some(SemanticTokensFullOptions::Bool(true)),
+    let result = InitializeResult {
+      capabilities: ServerCapabilities {
+        text_document_sync: Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
+          open_close: Some(true),
+          change: Some(TextDocumentSyncKind::FULL),
+          save: None,
+          ..Default::default()
+        })),
+
+        semantic_tokens_provider: Some(SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
+          SemanticTokensRegistrationOptions {
+            text_document_registration_options: TextDocumentRegistrationOptions {
+              document_selector: Some(vec![DocumentFilter {
+                language: Some("moonjuice".to_string()),
+                scheme: Some("file".to_string()),
+                pattern: None,
+              }]),
+            },
+            semantic_tokens_options: SemanticTokensOptions {
+              work_done_progress_options: Default::default(),
+              legend: get_legend(),
+              range: Some(true),
+              full: Some(SemanticTokensFullOptions::Bool(true)),
+            },
+            static_registration_options: Default::default(),
+          },
+        )),
+
+        ..Default::default()
       },
-    ));
+      ..Default::default()
+    };
 
     Ok(result)
   }
@@ -32,11 +60,45 @@ impl LanguageServer for Backend {
     self.client.log_message(MessageType::INFO, "Server initialised").await;
   }
 
+  async fn did_open(&self, params: DidOpenTextDocumentParams) {
+    self.documents.insert(
+      params.text_document.uri.to_string(),
+      Document::new(params.text_document.text),
+    );
+  }
+
+  async fn did_change(&self, params: DidChangeTextDocumentParams) {
+    if let Some(mut document) = self.documents.get_mut(params.text_document.uri.as_str()) {
+      document.value_mut().apply_change(params.content_changes);
+    }
+  }
+
+  async fn did_close(&self, params: DidCloseTextDocumentParams) {
+    self.documents.remove(params.text_document.uri.as_str());
+  }
+
   async fn semantic_tokens_full(
     &self,
     params: SemanticTokensParams,
   ) -> tower_lsp_server::jsonrpc::Result<Option<SemanticTokensResult>> {
-    self.semantic_highlighting_provider.highlight_full(params)
+    Ok(
+      self
+        .documents
+        .get(params.text_document.uri.as_str())
+        .map(|document| document.get_tokens_full()),
+    )
+  }
+
+  async fn semantic_tokens_range(
+    &self,
+    params: SemanticTokensRangeParams,
+  ) -> tower_lsp_server::jsonrpc::Result<Option<SemanticTokensRangeResult>> {
+    Ok(
+      self
+        .documents
+        .get(params.text_document.uri.as_str())
+        .map(|document| document.get_tokens_range(params.range)),
+    )
   }
 
   async fn shutdown(&self) -> tower_lsp_server::jsonrpc::Result<()> {
@@ -51,7 +113,7 @@ async fn main() {
 
   let (service, socket) = LspService::new(|client| Backend {
     client,
-    semantic_highlighting_provider: SemanticHighlightingProvider::new(),
+    documents: Default::default(),
   });
 
   Server::new(stdin, stdout, socket).serve(service).await;
--- packages/moonjuice-lsp/src/semantic_highlighting.rs
+++ packages/moonjuice-lsp/src/semantic_highlighting.rs
@@ -1,10 +1,6 @@
 use moonjuice_common::Position;
-use moonjuice_lexer::{Lexer, Token, TokenValue};
-use std::fs;
-use tower_lsp_server::ls_types::{
-  SemanticToken, SemanticTokenModifier, SemanticTokenType, SemanticTokens, SemanticTokensLegend, SemanticTokensParams,
-  SemanticTokensResult,
-};
+use moonjuice_lexer::{Token, TokenValue};
+use tower_lsp_server::ls_types::{SemanticToken, SemanticTokenModifier, SemanticTokenType, SemanticTokensLegend};
 
 enum TokenType {
   Parameter = 0,
@@ -18,98 +14,73 @@ enum TokenType {
   Operator = 8,
 }
 
-pub struct SemanticHighlightingProvider {}
-
-impl SemanticHighlightingProvider {
-  pub fn new() -> Self {
-    SemanticHighlightingProvider {}
+fn get_token_type(token: &Token) -> Option<TokenType> {
+  match token.value {
+    TokenValue::Nil => Some(TokenType::Keyword),
+    TokenValue::Bool(_) => Some(TokenType::Keyword),
+    TokenValue::Int(_) => Some(TokenType::Number),
+    TokenValue::Double(_) => Some(TokenType::Number),
+    TokenValue::String(_, _) => Some(TokenType::String),
+    TokenValue::Symbol(_) => Some(TokenType::Variable),
+    TokenValue::Keyword(_) => Some(TokenType::Keyword),
+    TokenValue::Operator(_) => Some(TokenType::Operator),
+    TokenValue::SpecialCharacter(_) => None,
+    TokenValue::Comment(_) => Some(TokenType::Comment),
+    TokenValue::UnexpectedCharacter(_) => None,
+    TokenValue::MalformedNumber(_) => Some(TokenType::Number),
+    TokenValue::MalformedString(_, _) => Some(TokenType::String),
   }
+}
 
-  pub fn get_legend(&self) -> SemanticTokensLegend {
-    SemanticTokensLegend {
-      token_types: vec![
-        SemanticTokenType::PARAMETER,
-        SemanticTokenType::VARIABLE,
-        SemanticTokenType::PROPERTY,
-        SemanticTokenType::FUNCTION,
-        SemanticTokenType::KEYWORD,
-        SemanticTokenType::COMMENT,
-        SemanticTokenType::STRING,
-        SemanticTokenType::NUMBER,
-        SemanticTokenType::OPERATOR,
-      ],
-      token_modifiers: vec![SemanticTokenModifier::READONLY],
-    }
+pub fn get_legend() -> SemanticTokensLegend {
+  SemanticTokensLegend {
+    token_types: vec![
+      SemanticTokenType::PARAMETER,
+      SemanticTokenType::VARIABLE,
+      SemanticTokenType::PROPERTY,
+      SemanticTokenType::FUNCTION,
+      SemanticTokenType::KEYWORD,
+      SemanticTokenType::COMMENT,
+      SemanticTokenType::STRING,
+      SemanticTokenType::NUMBER,
+      SemanticTokenType::OPERATOR,
+    ],
+    token_modifiers: vec![SemanticTokenModifier::READONLY],
   }
+}
 
-  pub fn highlight_full(
-    &self,
-    params: SemanticTokensParams,
-  ) -> tower_lsp_server::jsonrpc::Result<Option<SemanticTokensResult>> {
-    if let Some(file_path) = params.text_document.uri.to_file_path() {
-      let contents = fs::read_to_string(file_path)
-        .map_err(|err| tower_lsp_server::jsonrpc::Error::invalid_params(err.to_string()))?;
+pub fn convert_to_lsp_tokens<'a>(tokens: impl Iterator<Item = &'a Token>) -> Vec<SemanticToken> {
+  let mut previous_start = Position { line: 1, column: 1 };
 
-      let mut previous_start = Position { line: 1, column: 1 };
-      let tokens = Lexer::tokenise(contents.chars().collect())
-        .into_iter()
-        .filter_map(|token| {
-          let delta_line = if token.start.line != previous_start.line {
-            let delta_line = token.start.line - previous_start.line;
+  tokens
+    .filter_map(|token| {
+      let delta_line = if token.start.line != previous_start.line {
+        let delta_line = token.start.line - previous_start.line;
 
-            previous_start = Position {
-              line: token.start.line,
-              column: 1,
-            };
+        previous_start = Position {
+          line: token.start.line,
+          column: 1,
+        };
 
-            delta_line as u32
-          } else {
-            0
-          };
+        delta_line as u32
+      } else {
+        0
+      };
 
-          if let Some(token_type) = Self::get_token_type(&token) {
-            let semantic_token = SemanticToken {
-              delta_line,
-              delta_start: (token.start.column - previous_start.column) as u32,
-              length: token.lexeme.len() as u32,
-              token_type: token_type as u32,
-              token_modifiers_bitset: 0,
-            };
+      if let Some(token_type) = get_token_type(&token) {
+        let semantic_token = SemanticToken {
+          delta_line,
+          delta_start: (token.start.column - previous_start.column) as u32,
+          length: token.lexeme.len() as u32,
+          token_type: token_type as u32,
+          token_modifiers_bitset: 0,
+        };
 
-            previous_start = token.start;
-            Some(semantic_token)
-          } else {
-            None
-          }
-        })
-        .collect();
-
-      Ok(Some(SemanticTokensResult::Tokens(SemanticTokens {
-        result_id: None,
-        data: tokens,
-      })))
-    } else {
-      Err(tower_lsp_server::jsonrpc::Error::invalid_params(
-        "Non-file text document URIs are not supported",
-      ))
-    }
-  }
-
-  fn get_token_type(token: &Token) -> Option<TokenType> {
-    match token.value {
-      TokenValue::Nil => Some(TokenType::Keyword),
-      TokenValue::Bool(_) => Some(TokenType::Keyword),
-      TokenValue::Int(_) => Some(TokenType::Number),
-      TokenValue::Double(_) => Some(TokenType::Number),
-      TokenValue::String(_, _) => Some(TokenType::String),
-      TokenValue::Symbol(_) => Some(TokenType::Variable),
-      TokenValue::Keyword(_) => Some(TokenType::Keyword),
-      TokenValue::Operator(_) => Some(TokenType::Operator),
-      TokenValue::SpecialCharacter(_) => None,
-      TokenValue::Comment(_) => Some(TokenType::Comment),
-      TokenValue::UnexpectedCharacter(_) => None,
-      TokenValue::MalformedNumber(_) => Some(TokenType::Number),
-      TokenValue::MalformedString(_, _) => Some(TokenType::String),
-    }
-  }
+        previous_start = token.start;
+        Some(semantic_token)
+      } else {
+        None
+      }
+    })
+    .collect()
 }
